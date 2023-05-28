32dfde361d4a5bfa7dd10f30a20a1949791a1de0
vibrant001 <117892031+vibrant001@users.noreply.github.com>
2023-05-28 17:59:05 +0530

    spain_map

--- spain_main.js
+++ spain_main.js
@@ -1,12 +1,9 @@
 function showInfo(button) {
-    var infoCard = button.nextElementSibling;
-    infoCard.style.display = 'block';
-  }
-  
-  function hideInfo() {
-    var infoCards = document.getElementsByClassName('info-card');
-    for (var i = 0; i < infoCards.length; i++) {
-      infoCards[i].style.display = 'none';
-    }
-  }
-  
+  var infoCard = button.nextElementSibling;
+  infoCard.style.display = 'block';
+}
+
+function hideInfo(button) {
+  var infoCard = button.nextElementSibling;
+  infoCard.style.display = 'none';
+}
