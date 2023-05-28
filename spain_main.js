56661d876a16e6a74cdf4d3100f500d9fff71047
vibrant001 <117892031+vibrant001@users.noreply.github.com>
2023-05-28 19:01:47 +0530

    .

--- spain_main.js
+++ spain_main.js
@@ -7,3 +7,25 @@ function hideInfo(button) {
   var infoCard = button.nextElementSibling;
   infoCard.style.display = 'none';
 }
+
+
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.className);
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var className = event.dataTransfer.getData("text");
+  var button = document.querySelector("." + className);
+  
+  // Add your logic to handle the dropped button, e.g., saving it to the cart
+  // You can modify the cart icon or perform any other necessary actions here
+  
+  // Example: Add the button's class name to the cart element
+  var cart = document.querySelector(".cart-dropzone");
+  cart.textContent = className;
+}
