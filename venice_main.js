6a24ff7fa7264637950143aba3131a0ecbf76751
ggmxip <92012598+ggmxip@users.noreply.github.com>
2023-05-29 16:42:56 +0530

    venice done
    w

---
+++ venice_main.js
@@ -0,0 +1,37 @@
+
+function dragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.className);
+  }
+  
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var cartCount = document.getElementById("cartCount");
+    var count = parseInt(cartCount.innerText) + 1;
+    cartCount.innerText = count;
+  
+    var draggedButton = document.querySelector("." + data); // Get the dragged button
+    var correspondingInfoCard = draggedButton.nextElementSibling; // Get the corresponding info card
+  
+    event.target.appendChild(draggedButton);
+    correspondingInfoCard.style.display = "none"; 
+  }
+  
+  
+  
+  function showInfo(button) {
+    var infoCard = button.nextElementSibling;
+    infoCard.style.display = 'block';
+  }
+  
+  function hideInfo(button) {
+    var infoCard = button.nextElementSibling;
+    infoCard.style.display = 'none';
+  }
+  
+  
+  
